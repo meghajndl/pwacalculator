a15f663d604cdbea12ee0cc9de3ce3891e6245f8
meghajndl <78623267+meghajndl@users.noreply.github.com>
2021-06-28 15:40:40 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+'use strict';
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js');
+}
+
+var previousTheme = null;
+
+function setTheme(theme) {
+  var themeVars = {
+    displayId: 'calculator-display',
+    displayClass: 'calculator-display',
+    keyClass: 'calculator-key'
+  };
+  var displayEl = document.getElementById(themeVars.displayId);
+  var keyEls = document.querySelectorAll('.' + themeVars.keyClass);
+
+  displayEl.className = themeVars.displayClass + ' ' + theme;
+
+  keyEls.forEach((key) => {
+    if(previousTheme) {
+      key.classList.replace(previousTheme, theme);
+    } else {
+      key.classList.add(theme);
+    }
+  });
+
+  previousTheme = theme;
+  window.localStorage.setItem('theme', theme);
+}
+
+(function() {
+
+  var iPhone = (navigator.userAgent.indexOf("iPhone OS") !== -1);
+  var iPad = (navigator.userAgent.indexOf("iPad") !== -1);
+
+  var mainContainer = document.getElementById('calculator');
+  var instructions = document.getElementById('screen-instructions');
+  var modal = document.getElementById('modal');
+  var modalClose = document.getElementById('close-modal');
+  var modalOpen = document.getElementById('calculator-set-theme');
+
+  function showInstructions() {
+    instructions.classList.add('show');
+    mainContainer.classList.add('hide');
+  }
+
+  function initEvents() {
+    modalOpen.onclick = function(e) {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+    }
+
+    modalClose.onclick = function(e) {
+      modal.classList.remove('show');
+      modal.classList.add('hide');
+    }
+
+    window.onclick = function(e) {
+      if (e.target == modal) {
+        modal.classList.remove('show');
+        modal.classList.add('hide');
+      }
+    }
+  }
+
+  function init() {
+    initEvents();
+
+    var calc = new Calculator('.calculator-display', '.calculator-keys');
+    calc.initEvents();
+
+    var theme = window.localStorage.getItem('theme') || 'apple';
+    setTheme(theme);
+  }
+
+  if ((!window.navigator.standalone && (iPhone || iPad))) {
+    showInstructions();
+  } else {
+    init();
+  }
+
+})();
---
+++ calculator.js
@@ -0,0 +1,110 @@
+var Calculator = function(displayClass, keysClass) {
+  this.displayValue = '0';
+  this.firstOperand = null;
+  this.waitingForSecondOperand = false;
+  this.currentOperator = null;
+  this.currentOperatorClass = 'op-active';
+  this.display = document.querySelector(displayClass);
+  this.keys = document.querySelector(keysClass);
+  this.calculate = {
+    '/': (a, b) => (a / b),
+    '*': (a, b) => (a * b),
+    '+': (a, b) => (a + b),
+    '-': (a, b) => (a - b),
+    '=': (a, b) => b
+  };
+};
+
+Calculator.prototype = {
+
+  operator(nextOperator) {
+    const inputValue = parseFloat(this.displayValue);
+
+    if (this.currentOperator && this.waitingForSecondOperand)  {
+      this.currentOperator = nextOperator;
+      return;
+    }
+
+    if (this.firstOperand == null) {
+      this.firstOperand = inputValue;
+    } else if (this.currentOperator) {
+      const currentValue = this.firstOperand || 0;
+      const result = this.calculate[this.currentOperator](currentValue, inputValue);
+      this.displayValue = String(result);
+      this.firstOperand = result;
+    }
+
+    this.waitingForSecondOperand = true;
+    this.currentOperator = nextOperator;
+  },
+
+  digit(digit) {
+    if (this.waitingForSecondOperand === true) {
+      this.displayValue = digit;
+      this.waitingForSecondOperand = false;
+    } else {
+      this.displayValue = this.displayValue === '0' ? digit : this.displayValue + digit;
+    }
+  },
+
+  decimal(dot) {
+    if (this.waitingForSecondOperand === true) return;
+    // If the `displayValue` does not contain a decimal point
+    if (!this.displayValue.includes(dot)) {
+      // Append the decimal point
+      this.displayValue += dot;
+    }
+  },
+
+  clear() {
+    this.displayValue = '0';
+  },
+
+  allclear() {
+    this.displayValue = '0';
+    this.firstOperand = null;
+    this.waitingForSecondOperand = false;
+    this.currentOperator = null;
+  },
+
+  posneg() {
+    if(Math.sign(parseFloat(this.displayValue)) === 1) {
+      this.displayValue = '-' + this.displayValue;
+    } else {
+      this.displayValue = this.displayValue.replace('-', '');
+    }
+  },
+
+  updateDisplay() {
+    this.display.innerText = this.displayValue;
+  },
+
+  isOperatorBtn(el) {
+    var isOp = false;
+    if(typeof(el) === "object") {
+      if(el.getAttribute('data-type') == 'operator' && el.innerText !== '=') {
+        isOp = true;
+      }
+    }
+    return isOp;
+  },
+
+  initEvents() {
+    this.keys.addEventListener('click', (e) => {
+      for(var i in this.keys.children) {
+        var el = this.keys.children[i];
+        if(this.isOperatorBtn(el)) {
+          el.classList.remove(this.currentOperatorClass);
+        }
+      }
+      if(this.isOperatorBtn(e.target)) {
+        e.target.classList.add(this.currentOperatorClass);
+      }
+      if(this[e.target.getAttribute('data-type')]) {
+        this[e.target.getAttribute('data-type')](e.target.value);
+      }
+      this.updateDisplay();
+    });
+    this.updateDisplay();
+  }
+};
